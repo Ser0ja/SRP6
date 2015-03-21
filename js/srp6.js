d2cc126540abde9c9058fae74d6ad353c98150e6
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-22 00:48:19 +1300

    Added methods.

--- js/srp6.js
+++ js/srp6.js
@@ -1,35 +1,94 @@
-var rsp6 = function(){
-	/*----- CONSTANTS -----*/
-	var I = '', 	//User name
-		p = '', 	//Password
+var rsp6 = (function(){
+	/*----- VARIABLES -----*/
+	var I = 0, 	//User name
+		p = 0, 	//Password
+		s = 0, 	//Salt
 
-		s = '', 	//Salt
-		v = '', 	//Validation sum g ^ x % N
+		v = 0, 	//Validation sum g ^ x % N
 
 
-		N = '', 	//Safe prime
+		N = 0, 	//Safe prime
 
-		a = '', 	//Random number
+		a = 0, 	//Random number
 
-		k = '', 	//Multiplying constant H( N, g )
-		g = '', 	//Modulus generator
-		x = '', 	//Hash-salted password H( s, p )
+		k = 0, 	//Multiplying constant H( N, g )
+		g = 0, 	//Modulus generator
+		x = 0, 	//Hash-salted password H( s, p )
 
-		u = '', 	//Handshake fingerprint H( A, B )
+		u = 0, 	//Handshake fingerprint H( A, B )
 
-		K = '', 	//Client session key H(S)
 
-		A = '', 	//Mixed parties keys(Diffie-Helman) g % N
-		B = '', 	//Mixed parties keys(Diffie-Helman) k * v + g ^ b % N
+		A = 0, 	//Mixed parties keys(Diffie-Helman) g % N
+		B = 0, 	//Mixed parties keys(Diffie-Helman) k * v + g ^ b % N
 
-		Sc = '', 	//((B - k * ( g ^ x % N )) ^ ( a + u * x )) % N
-		Ss = '', 	//(( A * ( v ^ u % N )) ^ B ) % N
+		S = 0, 	//Session Id((B - k * ( g ^ x % N )) ^ ( a + u * x )) % N
+		K = 0, 	//Client session key H(S)
 
-		Mc = '', 	//Client confirmation message H( H(N) XOR H(g), H(I), S, A, B, K )
+		Mc = 0, //Client confirmation message H( H(N) XOR H(g), H(I), S, A, B, K )
 
-		Rc = '', 	//Client response H(A, Mc, Kc)
-		Rs = ''; 	//Server response H(A, Ms, Ks)
+		Rc = 0, //Client response H(A, Mc, Kc)
+		Rs = 0, //Server response H(A, Ms, Ks)
 
-	/*----- CONSTANTS -----*/
-	
-}
+	/*----- VARIABLES END -----*/
+
+	/*----- METHODS -----*/
+		calc = {
+
+			//Hash function
+			H: function(){
+				if(!arguments.length === 0){
+					var concat = 0;
+					for(var i = 0; i < arguments.length; i++){
+						concat = concat + arguments[i].toString(16);
+					}
+					return CryptoJS.SHA512(concat);
+
+				}
+				return undefined;
+			},
+
+		
+			v: function(){
+				//Validation sum g ^ x % N
+				return Math.pow(g, x) % N;
+			},
+			
+			A: function(){ 
+				//Mixed parties keys(Diffie-Helman) g % N
+				return g % N;
+			},
+			
+			B: function(){ 
+				//Mixed parties keys(Diffie-Helman) k * v + g ^ b % N
+				return k * v + Math.pow(g, b) % N;
+			},
+
+			u: function(){
+				return this.H(A, B);
+			},
+
+			x: function(){
+				return this.H(s, p);
+			},
+
+			S: function(){
+				//Session sum
+				return ( Math.pow( ( B - k * ( Math.pow(g, x) % N )), a + u*x ) ) % N;
+			},
+			
+			K: function(){
+				//Session key
+				return this.H(S)
+			},
+
+			m: function(){
+				return this.H( parseInt(this.H(N), 16) ^ parseInt(this.H(g), 16), this.H(I), S, A, B, K);
+			},
+
+			R: function(){
+				return H(A, Mc, K);
+			}
+		}
+	/*----- METHODS END -----*/
+
+}())
