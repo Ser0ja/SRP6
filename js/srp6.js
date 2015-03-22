578908a535d86ae0aabd842426ddb130362b69cf
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-22 18:56:33 +1300

    Updated variables and added hashing function.

--- js/srp6.js
+++ js/srp6.js
@@ -7,12 +7,12 @@ var rsp6 = (function(){
 		v = 0, 	//Validation sum g ^ x % N
 
 
-		N = 0, 	//Safe prime
+		N = parseInt('EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3' 16), 	//Safe prime
 
 		a = 0, 	//Random number
 
 		k = 0, 	//Multiplying constant H( N, g )
-		g = 0, 	//Modulus generator
+		g = 2, 	//Modulus generator
 		x = 0, 	//Hash-salted password H( s, p )
 
 		u = 0, 	//Handshake fingerprint H( A, B )
@@ -47,6 +47,22 @@ var rsp6 = (function(){
 				return undefined;
 			},
 
+			PRNG: function(){
+				var randomWords;
+				if (window.crypto && window.crypto.getRandomValues) {
+					//Use broser 
+					randomWords = new Int32Array(wordCount);
+					window.crypto.getRandomValues(randomWords);
+
+				}else if (window.msCrypto && window.msCrypto.getRandomValues) {
+
+					randomWords = new Int32Array(wordCount);
+					window.msCrypto.getRandomValues(randomWords);
+
+				}else {
+
+				}
+			},
 		
 			v: function(){
 				//Validation sum g ^ x % N
